4c25e3fb904c196cc38609aa64273bab05478532
Calvin Hall <calvin.hall@student.manchester.ac.uk>
2026-02-12 12:21:21 +0000

    Activation function can be specified when creating a neural network. Currently only supports sigmoid and ReLU

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,20 +1,30 @@
-from Neuron import Neuron, OutputNeuron
+from Neuron import *
+
+def sigmoid(number):
+    return 1/(1+exp(-number))
+
+def ReLU(number):
+    if number<0:
+        return 0
+    else:
+        return number
 
 class NeuralNetwork:
-    def __init__(self, structure):
+    def __init__(self, structure, activation):
         #structure is an array containing the number of neurons in each hidden layer
+        #activation is a string naming the activation function
 
         #construct 2D array, with each array containing the neurons for each layer, the last array containing only the output neuron
         self.layers=[[]]
         for layerSize in structure:
             if self.layers==[[]]:
                 for i in range(layerSize):
-                    self.layers[0].append(Neuron(2))
+                    self.layers[0].append(Neuron(2,activation))
                 previousLayerSize=layerSize
             else:
                 self.layers.append([])
                 for i in range(layerSize):
-                    self.layers[len(self.layers)-1].append(Neuron(previousLayerSize+1))#extra weight for bias
+                    self.layers[len(self.layers)-1].append(Neuron(previousLayerSize+1,activation))#extra weight for bias
                 previousLayerSize=layerSize
 
         #add output neuron to the array
@@ -24,14 +34,12 @@ class NeuralNetwork:
         self.biases=[0]*(len(structure)+1)#extra bias for output layer
 
     def calculate(self, value):
-        layerValues=[]
+        layerValues=[[value, self.biases[0]]]#set the first array to the input vector for the first hidden layer
         for layer in self.layers:
-            if layerValues==[]:
-                layerValues=[[value, self.biases[0]]]#set the first array to the input vector for the first hidden layer
-            layerValues.append([])
+            layerValues.append([])#create new empty array to hold outputs of the current layer
             for neuron in layer:
-                layerValues[len(layerValues)-1].append(neuron.calculate(layerValues[len(layerValues)-2]))
+                layerValues[len(layerValues)-1].append(neuron.calculate(layerValues[len(layerValues)-2]))#add each neuron's output to the array
             if len(layerValues)<=len(self.biases):
-                layerValues[len(layerValues)-1].append(self.biases[len(layerValues)-1])#add the bias for the next layer
+                layerValues[len(layerValues)-1].append(self.biases[len(layerValues)-1])#add the bias for the next layer to the array
 
-        return layerValues[len(layerValues)-1]
+        return layerValues
--- Neuron.py
+++ Neuron.py
@@ -1,8 +1,12 @@
 from numpy import random, dot, exp
+from functools import partial
 
 class Neuron:
-    def __init__(self, numberOfWeights):
+    def __init__(self, numberOfWeights, activation):
         #use random weight initialisation, using normal distribution with mean 0, s.d. 1
+        #activation is a string naming the activation function
+        self.activation=activation
+
         self.weights=[]
         for i in range(numberOfWeights):
             self.weights.append(random.normal(0,1))
@@ -11,12 +15,18 @@ class Neuron:
         #calculate dot product of weights and the values from the previous layer and the bias
         output=dot(inputVector,self.weights)
 
-        #use sigmoid function as activation function
-        output=1/(1+exp(-output))
+        #use partial function to call the activation function
+        output=self.activation(output)
 
         return output
     
 #exactly the same as any other neuron, but does not use an activation function
 class OutputNeuron(Neuron):
+    def __init__(self, numberOfWeights):
+        #random weight initialisation, no activation function
+        self.weights=[]
+        for i in range(numberOfWeights):
+            self.weights.append(random.normal(0,1))
+
     def calculate(self, inputVector):
         return dot(inputVector,self.weights)
