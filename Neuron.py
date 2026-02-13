bfffbb7ff3ed8551251cd6c75edeba36a68ce5c4
Calvin Hall <calvin.hall@student.manchester.ac.uk>
2026-02-13 10:13:41 +0000

    Improved some comments

--- Neuron.py
+++ Neuron.py
@@ -3,10 +3,11 @@ from collections.abc import Callable
 
 class Neuron:
     def __init__(self, numberOfWeights:int, activation:Callable[[float],float]):
-        #use random weight initialisation, using normal distribution with mean 0, s.d. 1
+        #numberOfWeights is an integer stating the number of weights necessary
         #activation is a string naming the activation function
         self.activation=activation
-
+        
+        #use random weight initialisation, using normal distribution with mean 0, s.d. 1
         self.weights=[]
         for i in range(numberOfWeights):
             self.weights.append(random.normal(0,1))
