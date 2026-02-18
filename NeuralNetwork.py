b2b920b8e3caae1adf00373acc305eb50d5d9f78
Calvin Hall <calvinraehall@icloud.com>
2026-02-18 22:45:34 +0000

    saveWeights and loadWeights are now saveParameters and loadParameters, and save/load the biases as well as the weights of each neuron

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -71,53 +71,54 @@ class NeuralNetwork:
         output = self.calculateAll(inputVector)
         return output[len(output)-1]#get only the values for the output neurons
     
-    def saveWeights(self, filePath:str)->None:
+    def saveParameters(self, filePath:str)->None:
         file=open(filePath,"w")
         for layer in self.layers:
             for neuron in layer:
                 for weight in neuron.weights:
                     file.write(str(weight))
-                    if weight is not neuron.weights[len(neuron.weights)-1]:#place a comma after every weight but the last one (in between all weights)
-                        file.write(",")
-                if neuron is not layer[len(layer)-1]:
+                    file.write(",")
+                file.write(str(neuron.bias))#store the bias at the end of the weights
+                if neuron is not layer[len(layer)-1]:#don't place a semicolon after the last neuron
                     file.write(";")#place a semicolon to separate weights for different neurons
             if layer is not self.layers[len(self.layers)-1]:
                 file.write("\n")#place a linebreak to separate neurons for different layers
         file.close()
 
-    def loadWeights(self, filePath:str)->None:
+    def loadParameters(self, filePath:str)->None:
         #read csv
         file=open(filePath,"r")
-        allWeights=file.read()
+        allParameters=file.read()
         file.close()
 
         #take the string from the file and format it into a useful data structure
-        allWeights=allWeights.split("\n")#separate all weights into layers
+        allParameters=allParameters.split("\n")#separate all weights into layers
         i=0
-        while i<len(allWeights):
-            allWeights[i]=allWeights[i].split(";")#separate layers into neurons
+        while i<len(allParameters):
+            allParameters[i]=allParameters[i].split(";")#separate layers into neurons
             j=0
-            while j<len(allWeights[i]):
-                allWeights[i][j]=allWeights[i][j].split(",")#separate neurons into weights
+            while j<len(allParameters[i]):
+                allParameters[i][j]=allParameters[i][j].split(",")#separate neurons into weights and a bias
                 k=0
-                while k<len(allWeights[i][j]):
-                    allWeights[i][j][k]=float(allWeights[i][j][k])
+                while k<len(allParameters[i][j]):
+                    allParameters[i][j][k]=float(allParameters[i][j][k])
                     k+=1
                 j+=1
             i+=1
 
-        #check that the stored weights and this neural network have the same numbers of layers and neurons in each layer
+        #check that the stored parameters and this neural network have the same numbers of layers and neurons in each layer
         sameStructure=True
-        if len(self.layers)!=len(allWeights):#check that number of layers are equal
+        if len(self.layers)!=len(allParameters):#check that number of layers are equal
             sameStructure=False
         else:
-            for layer, layerWeights in zip(self.layers,allWeights):
-                if len(layer)!=len(layerWeights):#check that number of neurons in each layer are equal
+            for layer, layerParameters in zip(self.layers,allParameters):
+                if len(layer)!=len(layerParameters):#check that number of neurons in each layer are equal
                     sameStructure=False
                     break
         
-        #set each neuron's weights to the stored weights
+        #set each neuron's parameters to the stored parameters
         if sameStructure:
-            for layer, layerWeights in zip(self.layers,allWeights):
-                for neuron, neuronWeights in zip(layer, layerWeights):
-                    neuron.weights=neuronWeights
+            for layer, layerParameters in zip(self.layers,allParameters):
+                for neuron, neuronParameters in zip(layer, layerParameters):
+                    neuron.bias=neuronParameters.pop()
+                    neuron.weights=neuronParameters
